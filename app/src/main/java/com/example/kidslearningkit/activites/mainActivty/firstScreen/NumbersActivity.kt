aa5087b8f3ceb9f8443f248fb3f6b2b383f9fd42
Lenovo <vedikasontakke2003@gmail.com>
2021-05-15 17:43:53 -0300

    initial commit

--- app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/NumbersActivity.kt
+++ app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/NumbersActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kidslearningkit.activites.mainActivty.firstScreen
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,24 +8,36 @@ import com.example.kidslearningkit.R
 import kotlinx.android.synthetic.main.activity_numbers.*
 
 class NumbersActivity : AppCompatActivity() ,  View.OnClickListener {
-   // var viewFlipper: ViewFlipper? = null
-   // var next: Button? = null
-  //  var previous: Button? = null
+
+    var songs: IntArray = intArrayOf(
+            R.raw.one , R.raw.two , R.raw.three , R.raw.four , R.raw.five , R.raw.six , R.raw.seven , R.raw.eight , R.raw.nine , R.raw.ten)
+
+  /*  var songs_number = Array<MediaPlayer>(
+            R.raw.one , R.raw.two , R.raw.three , R.raw.four , R.raw.five , R.raw.six , R.raw.seven , R.raw.eight , R.raw.nine , R.raw.ten)*/
+
+    var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_numbers)
 
-      //  viewFlipper = findViewById<View>(R.id.viewFlipper) as ViewFlipper
-     //   next = findViewById<View>(R.id.next) as Button
-     //   previous = findViewById<View>(R.id.previous) as Button
         right_number!!.setOnClickListener(this)
         left_number!!.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View) {
         if (v === right_number) {
             viewFlipper_numbers!!.showNext()
+
+            count++
+            var number : MediaPlayer = MediaPlayer.create(this , R.raw.eight)
+            number.start()
+
+
         } else if (v === left_number) {
             viewFlipper_numbers!!.showPrevious()
         }
-    }}
+    }
+
+}
