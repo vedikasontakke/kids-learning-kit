107e390ad86ff28e1b310e7f0f14349f8db68296
ParthTagalpallewar <55437315+PArth-tagalpallewar@users.noreply.github.com>
2021-05-15 19:15:29 +0530

    First Screen Sounds completed

--- app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/ShapesActivity.kt
+++ app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/ShapesActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kidslearningkit.activites.mainActivty.firstScreen
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,18 +9,82 @@ import kotlinx.android.synthetic.main.activity_fruits.*
 import kotlinx.android.synthetic.main.activity_shapes.*
 
 class ShapesActivity : AppCompatActivity()  , View.OnClickListener {
+
+    lateinit var songsList: ArrayList<Int>
+    var count = 0
+
+    lateinit var mediaPlayer: MediaPlayer
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shapes)
+
+        addSongs()
+
+        playSongByCount()
+
+        play_shape.setOnClickListener {
+            playSongByCount()
+        }
+        viewFlipper_shapes.setOnClickListener{
+            playSongByCount()
+        }
+
 
         right_shape!!.setOnClickListener(this)
         left_shape!!.setOnClickListener(this)
     }
 
+
+
+    private fun addSongs() {
+        songsList = arrayListOf(
+                R.raw.rectangle,
+                R.raw.circle,
+                R.raw.square,
+                R.raw.triangle,
+                R.raw.oval,
+                R.raw.hexagon,
+                R.raw.star,
+                R.raw.octagon,
+                R.raw.heart,
+                R.raw.pentagon
+        )
+    }
+
     override fun onClick(v: View) {
         if (v === right_shape) {
+            if (count == (songsList.size-1)) {
+                count = 0
+            } else {
+                count++
+            }
+
+            playSongByCount()
+
             viewFlipper_shapes!!.showNext()
         } else if (v === left_shape) {
+
+
+            if (count == 0) {
+                count = (songsList.size-1)
+            } else {
+                count--
+            }
+            playSongByCount()
+
             viewFlipper_shapes!!.showPrevious()
         }
-    }}
+    }
+    private fun playSongByCount() {
+        try {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+            }
+        } catch (e: UninitializedPropertyAccessException) {
+        }
+        mediaPlayer = MediaPlayer.create(this, songsList[count])
+        mediaPlayer.start()
+    }
+}
--- app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/VehiclesActivity.kt
+++ app/src/main/java/com/example/kidslearningkit/activites/mainActivty/firstScreen/VehiclesActivity.kt
@@ -1,31 +1,90 @@
 package com.example.kidslearningkit.activites.mainActivty.firstScreen
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.kidslearningkit.R
 import kotlinx.android.synthetic.main.activity_numbers.*
+import kotlinx.android.synthetic.main.activity_shapes.*
 import kotlinx.android.synthetic.main.activity_vehicles.*
 
 class VehiclesActivity : AppCompatActivity()  ,  View.OnClickListener {
-    // var viewFlipper: ViewFlipper? = null
-    // var next: Button? = null
-    //  var previous: Button? = null
+    lateinit var songsList: ArrayList<Int>
+    var count = 0
+
+    lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vehicles)
+        addSongs()
 
-        //  viewFlipper = findViewById<View>(R.id.viewFlipper) as ViewFlipper
-        //   next = findViewById<View>(R.id.next) as Button
-        //   previous = findViewById<View>(R.id.previous) as Button
+        playSongByCount()
+
+        play_vehicle.setOnClickListener {
+            playSongByCount()
+        }
+        viewFlipper_vehicle.setOnClickListener{
+            playSongByCount()
+        }
+
+
         right_vehicle!!.setOnClickListener(this)
         left_vehicle!!.setOnClickListener(this)
     }
 
+    private fun addSongs() {
+        songsList = arrayListOf(
+                R.raw.airplane,
+                R.raw.bus,
+                R.raw.car,
+                R.raw.helicopter,
+                R.raw.motorcycle,
+                R.raw.ship,
+                R.raw.truck,
+                R.raw.train,
+                R.raw.yacht,
+                R.raw.bicycle
+        )
+    }
+
     override fun onClick(v: View) {
         if (v === right_vehicle) {
+
+            if (count == (songsList.size-1)) {
+                count = 0
+            } else {
+                count++
+            }
+
+            playSongByCount()
+
             viewFlipper_vehicle!!.showNext()
         } else if (v === left_vehicle) {
+
+            if (count == 0) {
+                count = (songsList.size-1)
+            } else {
+                count--
+            }
+            playSongByCount()
+
             viewFlipper_vehicle!!.showPrevious()
         }
-    }}
+    }
+
+    private fun playSongByCount() {
+        try {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+            }
+        } catch (e: UninitializedPropertyAccessException) {
+        }
+        mediaPlayer = MediaPlayer.create(this, songsList[count])
+        mediaPlayer.start()
+    }
+}
+
+
+
